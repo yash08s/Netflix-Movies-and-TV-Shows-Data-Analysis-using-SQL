284b0733a4ed07a3b1cf14e795af56daea59d3f0
yash08s <yashsejwal2004@gmail.com>
2026-07-17 02:25:08 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -15,8 +15,7 @@ return top 5 year with highest avg content release!
 12. Find all content without a director
 13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
 14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-15.
-Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+15. Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
 the description field. Label content containing these keywords as 'Bad' and all other 
 content as 'Good'. Count how many items fall into each category.
 
